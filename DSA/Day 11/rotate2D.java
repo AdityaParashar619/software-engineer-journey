b04805099009991cd0bcfd67c2bc015d2910c84c
Aditya Parashar <parasharaditya895@gmail.com>
2026-06-28 15:09:54 +0530

    day 11 dsa question

---
+++ DSA/Day 11/rotate2D.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int temp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                temp[j][m-i-1]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=temp[i][j];
+            }
+        }
+    }
+}
